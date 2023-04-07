f684267e5e7031517ded8e791f80cb4ba53e58be
Uree1229 <101373376+Uree1229@users.noreply.github.com>
2023-04-07 20:33:51 +0900

    Update main.c

--- main.c
+++ main.c
@@ -1,16 +1,15 @@
 # define _CRT_SECURE_NO_WARNINGS
+
+#include "head.c"
 #include <stdio.h>
 #include <string.h>
 #include <stdlib.h>
-# define MAX_STACK_SIZE 100
 
+char** maze; // ë¯¸ë¡œ ë°ì´í„° ìˆ«ìë¡œ ë°”ê¿”ì„œ ì €ì¥
 int wid, hei;
 
-typedef Element;
-typedef struct {
-	int arr[MAX_STACK_SIZE];
-	int top;
-}Stack;
+
+
 
 int check(char string) {
 	if (string == EOF) {
@@ -25,75 +24,52 @@ int check(char string) {
 	
 }
 
-int IsFull(Stack* stack) {
-	if (stack->top == MAX_STACK_SIZE - 1) {
-		return 1;
-	}
-	else return 0;
-}
-int IsEmpty(Stack* stack) {
-	if (stack->top == -1) {
-		return 1;
-	}
-	else return 0;
-}
 
-void SettingStack(Stack* stack) {
-	stack->top = -1;
-}
-
-void Push(Stack* stack, int data) {
-	stack->arr[++(stack->top)] = data;
-}
-
-int Pop(Stack* stack) {
-	if (IsEmpty(stack)) {
-		printf("Is empty Stack\n");
-		return 0;
-	}
-	else
-		return stack->arr[stack->top];
-}
 int make_maze() {
 	char file_name[20];
-	printf("±æÀ» Ã£À» ¹Ì·ÎÀÇ ÆÄÀÏ¸íÀ» ÀÔ·ÂÇÏ½Ã¿À: ");
+	printf("ê¸¸ì„ ì°¾ì„ ë¯¸ë¡œì˜ íŒŒì¼ëª…ì„ ì…ë ¥í•˜ì‹œì˜¤: ");
 	scanf("%s", &file_name);
 	FILE* File = fopen(file_name, "r");
 	if (File == NULL) {
-		printf("Àß¸øµÈ ÆÄÀÏ¸í ÀÔ´Ï´Ù.");
+		printf("ì˜ëª»ëœ íŒŒì¼ëª… ì…ë‹ˆë‹¤.");
 	}
 	else {
 		fscanf(File, "%d %d", &wid, &hei);
 		wid = wid * 2 + 1; hei = hei * 2 + 1;
 		int i = 0;
-		char** size;
-		size = malloc(hei * sizeof(*size));
+
+		maze = malloc(hei * sizeof(*maze));
 		for (i = 0; i < hei; i++) {
-			size[i] = malloc(wid * sizeof(**size));
+			maze[i] = malloc(wid * sizeof(**maze));
 		}
 		fseek(File, 2, SEEK_CUR);
 		for (int h = 0; h < hei; h++) {
 			for (int w = 0; w < wid; w++) {
 				char b = fgetc(File);
-				size[h][w] = check(b);
-
+				maze[h][w] = check(b);
 
 			} fseek(File, 2, SEEK_CUR);
 		}
 		for (int i = 0; i < hei; i++) {
 			for (int j = 0; j < wid; j++) {
-				printf("%d", size[i][j]);
+				printf("%d", maze[i][j]);
 			}printf("\n");
 		}
 	}fclose(File);
 
 }
 
+void move(Stack * stack) {
+	if () {
+
+	}
+	
+}
+
 int main() {
+	Stack X, Y;
+	SettingStack(&X);
+	SettingStack(&Y);
 	make_maze();
-	
-	
-	
-	
 	
 }
